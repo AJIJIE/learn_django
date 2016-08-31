afff78e0d83994c387b59acb24d7e0f8eba7358e
jack <931473342@qq.com>
2016-08-31 19:13:42 +0800

    Signed-off-by: jack <931473342@qq.com>代码做了一部分整合，增加了前端的内容

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -2,9 +2,8 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from learn_django.view import hello
-from learn_django.db import db
-from learn_django import search
+
+
 
 
 urlpatterns = patterns('',
@@ -13,9 +12,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^hello/$','learn_django.view.hello',name='hello'),
-    url(r'^db/$','learn_django.db.db',name='db'),
-    url(r'^search_form/$','learn_django.search.search_form'),
-    url(r'^search/$','learn_django.search.search',name='search'),
-    url(r'^search_post/$','learn_django.search2.search_post',name='search_post'),
+    # url(r'^db/$','learn_django.view.db',name='db'),
+    url(r'^search_form/$','learn_django.view.search_form'),
+    url(r'^search/$','learn_django.view.search',name='search'),
+    url(r'^search_post/$','learn_django.view.search_post',name='search_post'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/$','learn_django.view.index',name='index'),
 )
--- learn_django/view.py
+++ learn_django/view.py
@@ -1,7 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+
+from django.http import HttpResponse
+
+# from app.models import db
+
 
 def hello(request):
     context      = {}
     context['hello']='python'
     return render(request,'hello.html',context)
+
+def index(request):
+    return render_to_response('index.html')
+
+def search_form(request):
+    return render_to_response('search_form.html')
+def search(request):
+    request.encoding='utf-8'
+    if 'q' in request.GET:
+        message='你搜索的内容为：'+request.GET['q'].encode('utf-8')
+    else:
+        message='你提交了空表单'
+    return HttpResponse(message)
+
+def search_post(request):
+    ctx={}
+    ctx.update(csrf(request))
+    if request.POST:
+        ctx['rlt']=request.POST['q']
+    return render(request,'post.html',ctx)
+# def db(request):
+# 	db1=db(name='w3cschool.cc')
+# 	db1.save()
+# 	return HttpResponse('<p>success</P>')
